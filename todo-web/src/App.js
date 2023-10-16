60e11ea8410fd214800e125b08c6daefe0303598
EC2 Default User <ec2-user@ip-172-31-25-100.ec2.internal>
2023-10-16 08:21:35 +0000

    ChakuraUI install

---
+++ todo-web/src/App.js
@@ -0,0 +1,14 @@
+import Task from './component/Task';
+
+const App = () =>{
+  return(
+    <>
+    <h1>タスク一覧</h1>
+    <Task name = "買い物" />
+    <Task name = "ランニング" />
+    <Task name = "プログラミングの勉強" />
+
+    </>
+    );
+}
+export default App;
